a5976e898a9d222e2db7d27ad4daad0c2c9f230c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-09 17:24:14 +0900

    feat: 호버 시 블러 및 재생 아이콘 추가

--- components/commons/Card.tsx
+++ components/commons/Card.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { IoPlaySharp } from 'react-icons/io5';
 
 interface IProps {
 	imageUrl: string;
@@ -9,20 +12,29 @@ interface IProps {
 }
 
 const Card = ({ imageUrl, title, link, size = 'large' }: IProps) => {
+	const [isHovered, setIsHovered] = useState(false);
+
 	return (
 		<Link
 			href={link}
-			className={`${
-				size === 'large' ? 'w-[450px]' : 'w-[180px]'
-			} flex flex-col cursor-pointer`}
+			className={`flex flex-col cursor-pointer relative`}
+			onMouseOver={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
 		>
-			<img
-				src={imageUrl}
-				alt={title}
-				className={`${
-					size === 'large' ? 'w-[450px] h-[225px]' : 'w-[180px] h-[180px]'
-				} rounded-2xl object-cover`}
-			/>
+			<div className={`relative ${size === 'small' ? 'w-full' : ''}`}>
+				{isHovered && (
+					<div className='w-full h-full bg-black absolute backdrop-blur-sm top-0 left-0 flex justify-center items-center opacity-70 z-50'>
+						<IoPlaySharp size={87} />
+					</div>
+				)}
+				<img
+					src={imageUrl}
+					alt={title}
+					className={`rounded-2xl object-cover ${
+						size === 'small' ? 'w-full h-full aspect-square' : ''
+					}`}
+				/>
+			</div>
 			<h4 className={`w-full text-[16px] line-clamp-1 mt-2`}>{title}</h4>
 		</Link>
 	);
