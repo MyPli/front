b7651989c29c054c781e8b162b13cc6d64ea42a4
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-09 16:07:29 +0900

    feat: 홈 페이지 구현

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,77 @@
-import Title from "@/components/commons/Title";
-import React from "react";
+import Card from '@/components/commons/Card';
+import Title from '@/components/commons/Title';
+import React from 'react';
 
 const Home = async () => {
-  return (
-    <div>
-      <Title text="인기 플레이리스트" />
-    </div>
-  );
+	return (
+		<div className='w-[calc(100dvw - 0px)]'>
+			<Title text='인기 플레이리스트' />
+			<div className='mt-[25px] flex w-[100%] justify-between'>
+				<Card
+					link=''
+					imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+					title='[KPOP Playlist] 짧은 케이팝 노동요'
+				/>
+				<Card
+					link=''
+					imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+					title='[KPOP Playlist] 짧은 케이팝 노동요'
+				/>
+				<Card
+					link=''
+					imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+					title='[KPOP Playlist] 짧은 케이팝 노동요'
+				/>
+			</div>
+			<div className='mt-20'>
+				<Title text='오늘의 최신 플레이리스트' className='font-normal' />
+				<div className='mt-[25px] flex w-[100%] justify-between'>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+					<Card
+						link=''
+						imageUrl='https://i.ytimg.com/vi/jVNyKwF5wL8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDnsEqKhhEYiP2E0stj3lq9Zj1Qgg'
+						title='[KPOP Playlist] 짧은 케이팝 노동요'
+						size='small'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
