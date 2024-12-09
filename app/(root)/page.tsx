7f05305b590a871879a0e9f118a5979e61fcfd11
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-09 17:23:50 +0900

    feat: grid gap 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,15 +13,15 @@ const Home = async () => {
 	return (
 		<div className='w-[calc(100vw-110px)]'>
 			<Title text='인기 플레이리스트' />
-			<div className='w-full mt-[25px] grid grid-cols-3 pr-0'>
+			<div className='mt-[25px] grid grid-cols-3 gap-[13px] w-[calc(100%-25px)]'>
 				{[dummy, dummy, dummy].map((item, i) => (
 					<Card key={item.title + i} size='large' {...item} />
 				))}
 			</div>
 			<div className='mt-20'>
 				<Title text='오늘의 최신 플레이리스트' className='font-normal' />
-				<div className='mt-[25px] grid grid-cols-7 w-[100%]'>
-					{[dummy, dummy, dummy, dummy, dummy, dummy, dummy].map((item, i) => (
+				<div className='mt-[25px] grid grid-cols-6 gap-[21px] w-[calc(100%-25px)]'>
+					{[dummy, dummy, dummy, dummy, dummy, dummy].map((item, i) => (
 						<Card key={item.title + i} size='small' {...item} />
 					))}
 				</div>
