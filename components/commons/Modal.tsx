4ca6f18613cf61bde68309eb0addf9afcec77e56
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-13 13:48:29 +0900

    fix: 모달 수정

--- components/commons/Modal.tsx
+++ components/commons/Modal.tsx
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
-import { IoCloseOutline, IoPencil } from "react-icons/io5";
+import { PropsWithChildren } from "react";
+import { IoCloseOutline } from "react-icons/io5";
 
 interface ModalProps {
   title?: string;
   isOpen: boolean;
   onClose: () => void;
-  children: React.ReactNode;
+  className?: string;
 }
 
-const Modal = ({ title, onClose, isOpen, children }: ModalProps) => {
+const Modal = ({
+  title,
+  onClose,
+  isOpen,
+  className,
+  children,
+}: PropsWithChildren<ModalProps>) => {
   if (!isOpen) return null;
 
   return (
     <div
       className="fixed top-0 left-0 w-screen h-screen 
-      flex-center bg-black bg-opacity-70 z-40
-      ho
-      "
+      flex-center bg-black bg-opacity-70 z-40 ho"
       onClick={onClose}
     >
       <div
-        className="bg-white rounded-md p-10 pt-6 
+        className={`bg-white rounded-md p-10 pt-6 
         min-w-96 min-h-72 flex flex-col gap-4 relative
-       box-border"
+       box-border ${className}`}
         onClick={(e) => e.stopPropagation()}
       >
         <header className="flex-between text-black ">
           {title ? (
-            <span className="text-md font-medium">{title}</span>
+            <span className="text-xl font-medium">{title}</span>
           ) : (
             <div className="bg-white w-1 h-1" />
           )}
