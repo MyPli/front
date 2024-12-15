43044eb08d9ac5747cb71326f1ffe6ca17a8d439
gunyongbok <qhrrjsdyd123@gmail.com>
2024-12-16 02:00:06 +0900

    feat: 검색 query 유무에 따른 검색 결과 없음 페이지 구현

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -7,11 +7,21 @@ const Search = async ({
   searchParams: Promise<{ q?: string }>;
 }) => {
   const query = (await searchParams).q;
-  console.log(query);
+
   return (
     <div>
-      <Title text="검색 결과" />
-      <div>{query}</div>
+      <Title text="검색" />
+      {query ? (
+        <div className="mt-4">
+          <p>검색어: {query}</p>
+        </div>
+      ) : (
+        <div className="flex justify-center items-start h-[calc(100vh-4rem)] p-4">
+          <p className="text-start text-gray-800 text-4xl">
+            검색 결과가 없습니다.
+          </p>
+        </div>
+      )}
     </div>
   );
 };
