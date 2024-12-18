2fb3daf0eb2a2501a1f5977158b577ca7630d6a2
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-18 18:01:33 +0900

    feat: 버튼 컴포넌트 분리

---
+++ components/commons/Button.tsx
@@ -0,0 +1,35 @@
+import React, { ButtonHTMLAttributes, PropsWithChildren } from 'react';
+
+
+interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string;
+  color: "primary" | "white" | 'border';
+}
+
+const Button = ({
+  className,
+  color,
+  children,
+  ...props
+}: PropsWithChildren<IProps>) => {
+  const getStyle = () => {
+    if (color === 'primary') {
+      return 'bg-primary text-white'
+    } else if (color === 'white') {
+      return 'bg-white text-primary'
+    } else if (color === 'border') {
+      return "border border-primary text-primary";
+    }
+  }
+
+  return (
+    <button
+      className={`rounded-xl ${getStyle()} text-base w-[83px] h-[40px] cursor-pointer ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
