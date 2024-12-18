7298c01b149fde8da2d41eae1dc783edb81beeb0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-18 18:02:52 +0900

    feat: 플레이리스트 삭제 모달 구현

---
+++ components/modals/DeletePlaylistModal.tsx
@@ -0,0 +1,41 @@
+import { useDeletePlaylistStore } from '@/store/useDeletePlaylistStore';
+import React from 'react';
+import Modal from '../commons/Modal';
+import Button from '../commons/Button';
+import { usePlaylist } from '@/hooks/usePlaylistState';
+
+const DeletePlaylistModal = ({ playlistId }: { playlistId: number }) => {
+  const { isDeleteModalOpen, closeDeleteModal } = useDeletePlaylistStore();
+  const { deletePlaylist } = usePlaylist(playlistId);
+
+  return (
+    <Modal
+      isOpen={isDeleteModalOpen}
+      onClose={closeDeleteModal}
+      className="min-h-44"
+      title="정말 삭제하시겠습니까?"
+    >
+      <h1 className="text-black text-[16px] mt-2">
+        해당 플레이리스트를 다시 복구할 수 없습니다.
+      </h1>
+      <div className="flex gap-[10px] mt-8">
+        <Button
+          color="border"
+          className="w-full h-[47px]"
+          onClick={closeDeleteModal}
+        >
+          취소
+        </Button>
+        <Button
+          color="primary"
+          className="w-full h-[47px]"
+          onClick={deletePlaylist}
+        >
+          삭제하기
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeletePlaylistModal;
