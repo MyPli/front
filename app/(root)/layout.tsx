0bbc7d98b77dd335600c37e05b8f4f2135f0bc03
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-19 17:38:40 +0900

    feat: 레이아웃에 재생바 추가

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
+'use client'
+
 import GnbList from "@/components/layout/gnb/GnbList";
 import SideList from "@/components/layout/sidebar/SideList";
+import SoundPlayer from '@/components/player/SoundPlayer';
+import { useControlPlayingStore } from '@/store/playStore';
+import { usePathname } from 'next/navigation';
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const avoidPages = ['/create', '/edit']
+  const { currentPlaylist } = useControlPlayingStore();
+
   return (
     <div className="bg-black text-white w-full min-h-screen">
       <GnbList />
@@ -10,6 +19,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <main className="ml-sideOpen flex-1 pt-mainTop pl-mainLeft pr-8">
         {children}
       </main>
+      {currentPlaylist.length > 0 && !avoidPages.includes(pathname) && <SoundPlayer />}
     </div>
   );
 };
