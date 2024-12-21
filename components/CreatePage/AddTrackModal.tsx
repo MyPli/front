d9236592683a3a0afee04704a65b0609148121c8
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-21 22:12:18 +0900

    fix: 데이터 추가 모달 구현

--- components/CreatePage/AddTrackModal.tsx
+++ components/CreatePage/AddTrackModal.tsx
@@ -4,32 +4,33 @@ import { HiMagnifyingGlass } from "react-icons/hi2";
 import CreatePlaylist from "./TrackForAdd";
 import { useForm } from "react-hook-form";
 import { useSearch, YoutubePlaylist } from '@/hooks/queries/useSearch';
-import { usePlaylistState } from '@/hooks/usePlaylistState';
 import { useAddVideoModalStore } from '@/store/addVideModalStore';
+import { useMakePlaylist } from '@/store/useMakePlaylist';
 
 interface FormData {
   searchText: string;
 }
 
-interface IProps {
-  playlistId: number;
-}
-
-const AddTrackModal = ({playlistId}: IProps) => {
+const AddTrackModal = () => {
   const { register, watch, getValues } = useForm<FormData>({
     mode: "onChange",
   });
   const { data: searchResult, refetch, isLoading } = useSearch(getValues('searchText'));
-  const { addVideo } = usePlaylistState(playlistId);
-  const {isAddModalOpen, closeAddModal } = useAddVideoModalStore();
+  const { isAddModalOpen, closeAddModal } = useAddVideoModalStore();
+  const { addTrack } = useMakePlaylist();
 
   useEffect(() => {
     refetch();
   }, [watch("searchText")]);
 
   const handleAddVideo = (item: YoutubePlaylist) => {
-    // TODO: addVideo API 수정되면 이것도 수정
-    addVideo?.({ ...item })
+    addTrack?.({
+      ...item,
+      id: Number(item.youtubeId),
+      url: item.source,
+      time: item.duration,
+      artist: item.channelName
+    });
     closeAddModal();
   }
 
@@ -49,14 +50,14 @@ const AddTrackModal = ({playlistId}: IProps) => {
         <HiMagnifyingGlass className="fill-black w-5 h-5 absolute right-2 top-[50%] transform -translate-y-1/2" />
       </div>
       <div className="h-[620px] overflow-auto">
-        {searchResult?.youtubePlaylists.map((item, i) => (
+        {searchResult?.youtubePlaylists?.map((item, i) => (
           <CreatePlaylist
             key={i}
             {...item}
             onAdd={() => handleAddVideo(item)}
           />
         ))}
-        {!searchResult?.youtubePlaylists.length && (
+        {!searchResult?.youtubePlaylists?.length && (
           <span>검색 결과가 없습니다.</span>
         )}
         {isLoading && <span>검색 중...</span>}
