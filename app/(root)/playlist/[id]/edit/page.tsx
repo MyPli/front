324662a00e6f9148ece8c9cccd180ffb6a27e8b0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-21 22:10:01 +0900

    feat: 수정 페이지 실행 전 데이터 전달

--- app/(root)/playlist/[id]/edit/page.tsx
+++ app/(root)/playlist/[id]/edit/page.tsx
@@ -1,7 +1,31 @@
+'use client'
+
 import Create from "@/components/CreatePage/CreatePage";
+import { usePlaylistState } from '@/hooks/usePlaylistState';
+import { useMakePlaylist } from '@/store/useMakePlaylist';
+import { useParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 const EditPlaylist = () => {
-	return <Create isEditMode />;
+  const params = useParams();
+  const playlistId = params.id;
+
+  const { playlistData } = usePlaylistState(Number(playlistId));
+  const { setPlaylistInfo, setTracks } = useMakePlaylist();
+
+  useEffect(() => {
+    if (playlistData) {
+      setPlaylistInfo({
+        ...playlistData,
+        count: playlistData?.videos?.length,
+        coverImage: playlistData?.coverImage || "",
+      });
+      setTracks(playlistData?.videos);
+      console.log(playlistData);
+    }
+  }, [playlistData]);
+
+  return <Create isEditMode />;
 };
 
 export default EditPlaylist;
