ec7aa158d8b808ab3e02b96c98998890c65ab015
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-21 14:58:30 +0900

    fix: 빌드 에러 수정

--- app/(root)/likes/page.tsx
+++ app/(root)/likes/page.tsx
@@ -7,6 +7,14 @@ import NoData from "@/components/commons/NoData";
 import Title from "@/components/commons/Title";
 import React from "react";
 
+interface Likes {
+  id: number;
+  title: string;
+  description: string;
+  coverImage: string;
+  tags: string[];
+}
+
 const Likelist = async () => {
   const session = await getAccessToken();
   const res = await getLikeList();
@@ -16,8 +24,8 @@ const Likelist = async () => {
       <Title text="내 좋아요 리스트" />
       {session && res.length > 0 ? (
         <div className="mt-[25px]  grid grid-cols-6 gap-[21px] w-[calc(100%-25px)]">
-          {res.map((item) => (
-            <Card key={item.id} size="small" {...item} />
+          {res.map((item: Likes) => (
+            <Card key={item.id} size="small" {...item} createdAt={''} />
           ))}
         </div>
       ) : (
--- app/(root)/myPlaylist/page.tsx
+++ app/(root)/myPlaylist/page.tsx
@@ -3,6 +3,7 @@ import { getMyPlaylists } from "@/action/playlist";
 import Card from "@/components/commons/Card";
 import NoData from "@/components/commons/NoData";
 import Title from "@/components/commons/Title";
+import { MyPlaylist } from '@/models/playlist.model';
 import React from "react";
 
 const MyPlayList = async () => {
@@ -14,7 +15,7 @@ const MyPlayList = async () => {
       <Title text="내 플레이리스트" />
       {session && res.length > 0 ? (
         <div className="mt-[25px]  grid grid-cols-6 gap-[21px] w-[calc(100%-25px)]">
-          {res.map((item) => (
+          {res.map((item: MyPlaylist) => (
             <Card key={item.id} size="small" {...item} />
           ))}
         </div>
