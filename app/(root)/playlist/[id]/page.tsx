1c292c9c489654fdda2d2a8a0f6f0aa1a048239c
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-21 12:44:18 +0900

    feat: 상세 플레이리스트에 로딩 문구 추가, 코드 스타일 수정

--- app/(root)/playlist/[id]/page.tsx
+++ app/(root)/playlist/[id]/page.tsx
@@ -15,11 +15,13 @@ import { IoPlaySharp } from 'react-icons/io5';
 const DetailPlaylist = () => {
 	const params = useParams();
 	const playlistId = Number(params.id);
-	const navigate = useRouter()
+  const navigate = useRouter()
+  
+  const isMyPlaylist = false;
 	
 	const { setPlaylistInfo } = useMakePlaylist();
 	const { isDeleteModalOpen, openDeleteModal } = useDeletePlaylistStore();
-  const { playlistData } = usePlaylistState(playlistId);
+  const { playlistData, loadingPlaylist } = usePlaylistState(playlistId);
   const { setCurrentPlaylist, setCurrentVideoIndex } = useControlPlayingStore();
 
 	const handleNavigateToEdit = () => {
@@ -33,12 +35,12 @@ const DetailPlaylist = () => {
 		navigate.push(`/playlist/${playlistId}/edit`);
   }
 
-    const handlePlay = (startIndex?: number) => {
-      setCurrentPlaylist(playlistData?.videos ?? []);
-      if (startIndex !== undefined) {
-        setCurrentVideoIndex(startIndex);
-      }
-    };
+  const handlePlay = (startIndex?: number) => {
+    setCurrentPlaylist(playlistData?.videos ?? []);
+    if (startIndex !== undefined) {
+      setCurrentVideoIndex(startIndex);
+    }
+  };
 
   const handleClickTrack = (trackIndex: number) => {
     handlePlay(trackIndex);
@@ -47,76 +49,105 @@ const DetailPlaylist = () => {
   return (
     <>
       {isDeleteModalOpen && <DeletePlaylistModal playlistId={playlistId} />}
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center pb-24">
         <div className="min-w-[1190px] relative">
-          <div className="absolute right-10 flex gap-[16px] z-10 top-10">
-            <Button color="white" onClick={openDeleteModal}>
-              삭제
-            </Button>
-            <Button color="white" onClick={handleNavigateToEdit}>
-              수정
-            </Button>
-          </div>
-          <div className="h-[400px] w-full relative mb-10 flex jusitfy-center">
-            <div className="absolute w-full top-0 flex justify-center items-center mt-10 gap-[46px]">
-              <Image
-                src={playlistData?.coverImage as string}
-                alt=""
-                width={300}
-                height={300}
-                className="w-[300px] h-[300px] cursor-pointer rounded-full object-cover"
-              />
-              <div className="min-w-[504px] relative flex flex-col justify-center">
-                <span className="text-[16px] cursor-pointer">{playlistData?.createdBy}</span>
-                <h3 className="text-[30px] max-h-[120px] mb-6 mt-2">
-                  {playlistData?.title as string}
-                </h3>
-
-                <div className="flex flex-col gap-[20px]">
-                  <div className="mt-[60px] flex items-center gap-[8px]">
-                    {playlistData?.tags.map((tag) => (
-                      <div key={tag} className="flex gap-[3px] cursor-pointer">
-                        <span>#</span>
-                        <span>{tag}</span>
-                      </div>
-                    ))}
-                  </div>
-                  <div className="flex gap-[20px] text-white text-[16px]">
-                    <span>총 시간 {playlistData?.totalTime}</span>
-                    <span>곡 수: {playlistData?.videos.length}곡</span>
-                  </div>
-                </div>
-              </div>
-              <button
-                className="absolute w-[60px] h-[60px] bg-primary rounded-full flex justify-center items-center pl-2 shadow-lg hover:shadow-xl transition-shadow right-20 bottom-12"
-                onClick={() => handlePlay(0)}
-              >
-                <IoPlaySharp color="white" className="w-[40px] h-[40px]" />
-              </button>
+          {loadingPlaylist || !playlistData?.title ? (
+            <div className="h-[400px] w-full flex items-center justify-center">
+              <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
             </div>
-            <Image
-              src="/blur.png"
-              alt=""
-              width={400}
-              height={400}
-              className="h-[400px] w-[1190px] object-cover"
-            />
-          </div>
-          <div className="w-full flex flex-col items-center mt-[55px] flex gap-[10px]">
-            {playlistData?.videos.length ? (
-              playlistData?.videos.map((track: Video, i) => (
-                <Track
-                  key={track.title + i}
-                  {...track}
-                  onClick={() => handleClickTrack(i)}
+          ) : (
+            <>
+              {isMyPlaylist && (
+                <div className="absolute right-20 flex gap-[16px] z-10 top-10">
+                  <Button color="white" onClick={openDeleteModal}>
+                    삭제
+                  </Button>
+                  <Button color="white" onClick={handleNavigateToEdit}>
+                    수정
+                  </Button>
+                </div>
+              )}
+              <div className="h-[400px] w-full relative mb-10 flex jusitfy-center items-center">
+                <div className="absolute w-full top-0 flex justify-start items-center gap-[46px] mt-10 pl-40">
+                  {playlistData?.coverImage ? (
+                    <Image
+                      src={playlistData?.coverImage as string}
+                      alt=""
+                      width={300}
+                      height={300}
+                      className="w-[300px] h-[300px] cursor-pointer rounded-full object-cover"
+                    />
+                  ) : (
+                    <div
+                      className={`rounded-full w-[300px] h-[300px] bg-[#545454] flex justify-center items-center`}
+                    >
+                      <Image
+                        src="/logo-white.svg"
+                        alt="no images"
+                        width={100}
+                        height={50}
+                      />
+                    </div>
+                  )}
+                  <div className="min-w-[504px] relative flex flex-col justify-center">
+                    <span className="text-[16px] cursor-pointer">
+                      {playlistData?.createdBy}
+                    </span>
+                    <h3 className="text-[30px] max-h-[120px] mb-6 mt-2">
+                      {playlistData?.title as string}
+                    </h3>
+
+                    <div className="flex flex-col gap-[20px]">
+                      <div className="mt-[60px] flex items-center gap-[8px]">
+                        {playlistData?.tags?.map((tag) => (
+                          <div
+                            key={tag}
+                            className="flex gap-[3px] cursor-pointer"
+                          >
+                            <span>#</span>
+                            <span>{tag}</span>
+                          </div>
+                        ))}
+                      </div>
+                      <div className="flex gap-[20px] text-white text-[16px]">
+                        <span>총 시간 {playlistData?.totalTime}</span>
+                        <span>곡 수: {playlistData?.videos?.length}곡</span>
+                      </div>
+                    </div>
+                  </div>
+                  <button
+                    className="absolute w-[60px] h-[60px] bg-primary rounded-full flex justify-center items-center pl-2 shadow-lg hover:shadow-xl transition-shadow right-20 bottom-12"
+                    onClick={() => handlePlay(0)}
+                  >
+                    <IoPlaySharp color="white" className="w-[40px] h-[40px]" />
+                  </button>
+                </div>
+                <Image
+                  src="/blur.png"
+                  alt=""
+                  width={400}
+                  height={400}
+                  className="h-[400px] w-full object-cover"
                 />
-              ))
-            ) : (
-              <div className="mt-[150px] flex justify-center items-center text-base">
-                음악이 없습니다.
               </div>
-            )}
-          </div>
+              <div className="w-full flex flex-col items-center mt-[55px] flex gap-[10px]">
+                {playlistData?.videos?.length ? (
+                  playlistData?.videos?.map((track: Video, i) => (
+                    <Track
+                      key={track.title + i}
+                      {...track}
+                      onClick={() => handleClickTrack(i)}
+                      isMyPlaylist={isMyPlaylist}
+                    />
+                  ))
+                ) : (
+                  <div className="mt-[150px] flex justify-center items-center text-base">
+                    음악이 없습니다.
+                  </div>
+                )}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
