d5f6cfffbd922bb821cdffdb27831af80a275255
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-12-21 12:43:27 +0900

    feat: 메인페이지에 로딩 문구 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,18 +11,30 @@ const Home = () => {
 	return (
     <div className="w-[calc(100vw-110px)]">
       <Title text="인기 플레이리스트" />
-      <div className="mt-[25px] grid grid-cols-3 gap-[13px] w-[calc(100%-25px)]">
-        {populars?.map((item, i) => (
-          <Card key={item.title + i} size="large" {...item} />
-        ))}
-      </div>
-      <div className="mt-20 pb-10">
-        <Title text="오늘의 최신 플레이리스트" className="font-normal" />
-        <div className="mt-[25px] grid grid-cols-6 gap-[21px] w-[calc(100%-25px)]">
-          {latests?.map((item, i) => (
-            <Card key={item.title + i} size="small" {...item} />
+      {populars && populars.length > 0 ? (
+        <div className="mt-[25px] grid grid-cols-3 gap-[13px] w-[calc(100%-25px)]">
+          {populars?.map((item, i) => (
+            <Card key={item.title + i} size="large" {...item} />
           ))}
         </div>
+      ) : (
+        <div className="mt-[25px] grid grid-cols-3 gap-[13px] w-[calc(100%-25px)]">
+          Loading...
+        </div>
+      )}
+      <div className="mt-20 pb-10">
+        <Title text="오늘의 최신 플레이리스트" className="font-normal" />
+        {latests && latests.length > 0 ? (
+          <div className="mt-[25px] grid grid-cols-6 gap-[21px] w-[calc(100%-25px)]">
+            {latests?.map((item, i) => (
+              <Card key={item.title + i} size="small" {...item} />
+            ))}
+          </div>
+        ) : (
+          <div className="mt-[25px] grid grid-cols-3 gap-[13px] w-[calc(100%-25px)]">
+            Loading...
+          </div>
+        )}
       </div>
     </div>
   );
